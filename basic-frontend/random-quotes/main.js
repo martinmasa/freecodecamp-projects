d73a3f4a5ac6b2fed53867ef70e9065181ab57f3
Martin M <mxa.code@gmail.com>
2015-12-15 22:44:36 +0000

    Random Quotes: render random quotes + add functionality to tweet quote

--- basic-frontend/random-quotes/main.js
+++ basic-frontend/random-quotes/main.js
@@ -1,35 +1,67 @@
-
-
-var randomQuotes = (function(window, document, undefined) {
+(function() {
 
   'use strict';
 
   var quotes = [
-    'When you jump for joy, beware that no one moves the ground from beneath your feet. // Stanislaw J. Lec',
-    'Never go to bed mad. Stay up and fight. // Phyllis Diller',
-    'Few things are harder to put up with than the annoyance of a good example. // Mark Twain',
-    'Wit makes its own welcome, and levels all distinctions. No dignity, no learning, no force of character, can make any stand against good wit. // Ralph Waldo Emerson',
-    'The best way to keep one\'s word is not to give it. // Napoleon Bonaparte',
-    'Every man is the builder of a temple called his body. // Henry David Thoreau',
-    'All people want is someone to listen. // Hugh Elliot',
-    'It\'s not enough that we do our best; sometimes we have to do what\'s required. // Sir Winston Churchill',
-    'Friendship with oneself is all-important, because without it one cannot be friends with anyone else in the world. // Eleanor Roosevelt',
-    'Keeping your clothes well pressed will keep you from looking hard pressed. // Coleman Cox'
+    {
+      text: 'When you jump for joy, beware that no one moves the ground from beneath your feet.',
+      author: 'Stanislaw J. Lec',
+    },
+    {
+      text: 'Never go to bed mad. Stay up and fight.',
+      author: 'Phyllis Diller',
+    },
+    {
+      text: 'Few things are harder to put up with than the annoyance of a good example.',
+      author: 'Mark Twain',
+    },
+    {
+      text: 'Wit makes its own welcome, and levels all distinctions. No dignity, no learning, no force of character, can make any stand against good wit.',
+      author: 'Ralph Waldo Emerson'
+    },
+    {
+      text: 'The best way to keep one\'s word is not to give it.',
+      author: 'Napoleon Bonaparte',
+    },
+    {
+      text: 'Every man is the builder of a temple called his body.',
+      author: 'Henry David Thoreau',
+    },
+    {
+      text: 'All people want is someone to listen.',
+      author: 'Hugh Elliot',
+    },
+    {
+      text: 'It\'s not enough that we do our best; sometimes we have to do what\'s required.',
+      author: 'Sir Winston Churchill',
+    },
+    {
+      text: 'Friendship with oneself is all-important, because without it one cannot be friends with anyone else in the world.',
+      author: 'Eleanor Roosevelt'},
+    {
+      text: 'Keeping your clothes well pressed will keep you from looking hard pressed.',
+      author: 'Coleman Cox'
+    }
   ];
+
   var currentQuote;
 
-
-  // cache DOM
   var $el = $('#random-quotes');
-  var $quote = $el.find('#quote-container');
+  var $quote = $el.find('blockquote p.quote-text ');
+  var $author = $el.find('cite');
   var $button = $el.find('button#next-quote');
+  var $tweetBtn = $el.find('a#tweet-quote');
+  var tweetIntentUrl = "https://twitter.com/intent/tweet?text=";
 
-  // bind events
   $button.on('click', getNextQuote);
 
-  // render
-  function _render () {
+  getNextQuote();
 
+  function _render () {
+    $quote.text(currentQuote.text);
+    $author.text(currentQuote.author);
+    var tweetQuoteUrl = [tweetIntentUrl, currentQuote.text, ' - ', currentQuote.author];
+    $tweetBtn.attr('href', tweetQuoteUrl.join('') );
   }
 
   function getNextQuote () {
@@ -37,8 +69,4 @@ var randomQuotes = (function(window, document, undefined) {
      _render();
   }
 
-  return {
-    getNextQuote: getNextQuote
-  };
-
-})(window, document);
+})();
